f33d5845b35d9eed828b3c80355ae88a649483f5
EVAN96KWON <koun013@naver.com>
2021-08-05 17:27:18 +0900

    programmers 1844

---
+++ programmers/42885/42885.py
@@ -0,0 +1,10 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution([70, 50, 80, 50], 100))  # 3
+    print(solution([70, 80, 50], 100))  # 3
