18a0f5d4e39c33fe7ad67ed6745250fccf004123
EVAN96KWON <koun013@naver.com>
2021-08-09 15:38:29 +0900

    programmers 42883

--- programmers/42883/42883.py
+++ programmers/42883/42883.py
@@ -1,12 +1,16 @@
 def solution(number, k):
     stack = [number[0]]
 
+    # 숫자 순회
     for i in range(1, len(number)):
-        while stack and stack[-1] < number[i] and k > 0:
+        # 스택이 비지 않은 동안, k >0 일때 동안, 스택의 제일 끝이 i번째 수보다 작을 때
+        while stack and k > 0 and stack[-1] < number[i]:
             k -= 1
             stack.pop()
+        # 스택에 i번째 수 추가
         stack.append(number[i])
 
+    # 만약 k가 0이 아니라면, 남은 부분은 삭제
     stack = stack[:-k] if k != 0 else stack
 
     return ''.join(stack)
