dc394255fe4a0093dab3406704106f840d3faddc
EVAN9416KWON <koun012@naver.com>
2022-01-07 03:14:29 +0900

    baekjoon 1235

---
+++ backjun/1235.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def check(N, list, s):
+    for i in range(N):
+        for j in range(i + 1, N):
+            if list[i][-s:] == list[j][-s:]:
+                return False
+    return True
+
+
+def solution(N, list):
+    for i in range(1, len(list[0])):
+        if check(N, list, i):
+            return i
+    return len(list[0])
+
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline())
+    list = [sys.stdin.readline().strip() for _ in range(N)]
+
+    print(solution(N, list))
