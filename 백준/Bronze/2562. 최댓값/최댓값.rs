5de4b96495e82a87855af4767839a15d136e04e7
heonjin <87924725+kwon-evan@users.noreply.github.com>
2023-09-20 11:47:24 +0900

    [Bronze III] Title: 최댓값, Time: 4 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    for _ in 0..9 {
+        io::stdin().read_line(&mut input).unwrap();
+    }
+    let numbers: Vec<i32> =
+        Vec::from_iter(input.split_whitespace().map(|x| x.parse::<i32>().unwrap()));
+
+    let max = numbers.iter().max().unwrap();
+    let max_idx = numbers.iter().position(|x| x == max).unwrap();
+    println!("{}", max);
+    println!("{}", max_idx + 1);
+}
