5a25391281b8cffc3bbf67f66ee5caf6214a176b
heonjin <87924725+kwon-evan@users.noreply.github.com>
2023-09-20 11:10:31 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 4 ms, Memory: 13148 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.rs
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.rs
@@ -1,12 +1,13 @@
+use std::{io, iter};
+
 fn main() {
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let number = input.trim().parse::<u8>().unwrap();
+    io::stdin().read_line(&mut input).unwrap();
+    let number = input.trim().parse::<usize>().unwrap();
 
-    for i in 0..number {
-        for _ in 0..(number - i) {
-            print!("*");
-        }
-        println!();
-    }
+    let stars: String = (0..number)
+        .flat_map(|i| iter::repeat("*").take(number - i).chain(iter::once("\n")))
+        .collect();
+
+    println!("{}", stars);
 }
