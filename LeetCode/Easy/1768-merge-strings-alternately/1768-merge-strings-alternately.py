c07f3f34b66d38b9660bf61c0c876de5fa1ecfd2
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-01-02 21:07:19 +0900

    Time: 74 ms (5.24%), Space: 17.4 MB (8.06%) - LeetHub

---
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        word = deque()
+
+        word1 = deque(word1)
+        word2 = deque(word2)
+
+        while word1 and word2:
+            word.append(word1.popleft())
+            word.append(word2.popleft())
+
+        word = word + word1 + word2
+
+        return "".join(word)
