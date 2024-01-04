ffd8681fa60e329779eb9d0b09145f4aec6d60b2
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-01-04 13:33:07 +0900

    Time: 0 ms (100.00%), Space: 2.5 MB (68.44%) - LeetHub

--- LeetCode/Easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.rs
+++ LeetCode/Easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.rs
@@ -1,25 +1,24 @@
 impl Solution {
     pub fn reverse_vowels(s: String) -> String {
-        let mut gathers = vec![];
-
-        for c in s.chars() {
-            if "aeiouAEIOU".contains(c) {
-                gathers.push(c);
+        let mut result: Vec<char> = s.chars().collect();
+        let (mut i, mut j) = (0, s.len() - 1);
+        while i < j {
+            if Self::is_vowel(result[i]) {
+                while j > i {
+                    if Self::is_vowel(result[j]) {
+                        (result[i], result[j]) = (result[j], result[i]);
+                        j -= 1;
+                        break;
+                    }
+                    j -= 1;
+                }
             }
+            i += 1;
         }
-        gathers.reverse();
+        result.iter().collect()
+    }
 
-        let mut result = String::from("");
-
-        for c in s.chars() {
-            if "aeiouAEIOU".contains(c) {
-                result.push(gathers.remove(0));
-            } else {
-                result.push(c);
-            }
-        }
-
-        result
+    pub fn is_vowel(c: char) -> bool {
+        "aeiouAEIOU".contains(c)
     }
 }
- 
