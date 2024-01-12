7edca4d7d3fb79e1c4591ab474786ef568e67b71
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-01-12 11:26:50 +0900

    Time: 0 ms (100.00%), Space: 2.2 MB (38.69%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.rs
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        let roman: HashMap<char, i32> = HashMap::from([
+        let roman = HashMap::from([
             ('I', 1),
             ('V', 5),
             ('X', 10),
@@ -11,22 +11,17 @@ impl Solution {
             ('D', 500),
             ('M', 1000),
         ]);
-        let mut result = 0;
-        let chars: Vec<char> = s.chars().collect();
+        let mut res = 0;
 
-        for i in 0..chars.len() {
-            let current_value = *roman.get(&chars.get(i).cloned().unwrap()).unwrap_or(&0);
-            let next_value = *roman
-                .get(&chars.get(i + 1).cloned().unwrap_or('\0'))
-                .unwrap_or(&0);
-
-            result += if current_value < next_value {
-                -current_value
+        for i in 0..s.len() {
+            if i + 1 < s.len()
+                && roman[&s.chars().nth(i).unwrap()] < roman[&s.chars().nth(i + 1).unwrap()]
+            {
+                res -= roman[&s.chars().nth(i).unwrap()];
             } else {
-                current_value
-            };
+                res += roman[&s.chars().nth(i).unwrap()];
+            }
         }
-
-        result
+        res
     }
 }
