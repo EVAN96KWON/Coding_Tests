649861595fc94ebe344c6ce2321154d1b576cebb
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-01-12 11:32:06 +0900

    Time: 0 ms (100.00%), Space: 2.3 MB (14.52%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.rs
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.rs
@@ -1,27 +1,26 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        let roman = HashMap::from([
-            ('I', 1),
-            ('V', 5),
-            ('X', 10),
-            ('L', 50),
-            ('C', 100),
-            ('D', 500),
-            ('M', 1000),
-        ]);
         let mut res = 0;
 
         for i in 0..s.len() {
-            if i + 1 < s.len()
-                && roman[&s.chars().nth(i).unwrap()] < roman[&s.chars().nth(i + 1).unwrap()]
-            {
-                res -= roman[&s.chars().nth(i).unwrap()];
-            } else {
-                res += roman[&s.chars().nth(i).unwrap()];
-            }
+            let cur = Self::get_roman(s.chars().nth(i).unwrap());
+            let next = Self::get_roman(s.chars().nth(i + 1).unwrap_or('0'));
+
+            res += if cur < next { -cur } else { cur }
         }
         res
+    }
+
+    fn get_roman(roman_char: char) -> i32 {
+        match roman_char {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
+        }
     }
 }
