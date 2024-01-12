d7feefdfa5c8319c5adb39e7cbe74e6b12905a0a
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-01-12 11:32:49 +0900

    Time: 0 ms (100.00%), Space: 2.1 MB (38.69%) - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.rs
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.rs
@@ -1,12 +1,13 @@
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
         let mut res = 0;
+        let chars: Vec<char> = s.chars().collect();
 
-        for i in 0..s.len() {
-            let cur = Self::get_roman(s.chars().nth(i).unwrap());
-            let next = Self::get_roman(s.chars().nth(i + 1).unwrap_or('0'));
+        for i in 0..chars.len() {
+            let cur = Self::get_roman(chars[i]);
+            let next = Self::get_roman(chars.get(i + 1).cloned().unwrap_or('0'));
 
-            res += if cur < next { -cur } else { cur }
+            res += if cur < next { -cur } else { cur };
         }
         res
     }
