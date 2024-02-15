f309ce1e532435664ad5e42728eacb9a5ad20cba
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-02-15 10:32:20 +0900

    Time: 0 ms (100.00%), Space: 2.2 MB (46.38%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.rs
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.rs
@@ -1,13 +1,13 @@
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        if nums.len() == 1 {
-            return nums[0];
+        let (mut prev, mut prevprev) = (0, 0);
+
+        for n in nums {
+            let tmp = prev.max(n + prevprev);
+            prevprev = prev;
+            prev = tmp;
         }
 
-        let mut dp = Vec::from([nums[0], nums[1].max(nums[0])]);
-        for i in 2..nums.len() {
-            dp.push(dp[i - 1].max(dp[i - 2] + nums[i]));
-        }
-        dp.last().unwrap().clone()
+        prev
     }
 }
