ea054d9e27143343d4cc67aebfcc5025a9bb93d8
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-02-16 14:30:25 +0900

    Time: 0 ms (100.00%), Space: 2.1 MB (89.08%) - LeetHub

---
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {
+        let mut dp = vec![vec![1; n as usize]; m as usize];
+
+        for i in 1..m as usize {
+            for j in 1..n as usize {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[(m - 1) as usize][(n - 1) as usize];
+    }
+}
