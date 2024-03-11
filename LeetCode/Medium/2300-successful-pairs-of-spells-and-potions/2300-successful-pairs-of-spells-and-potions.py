f291cca8d95b4fb056229df5c7981a5157b2a79a
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-03-11 17:00:57 +0900

    Time: 874 ms (85.6%), Space: 39.6 MB (57.45%) - LeetHub

---
+++ LeetCode/Medium/2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,11 @@
+class Solution:
+    def successfulPairs(
+        self,
+        spells: List[int],
+        potions: List[int],
+        success: int,
+    ) -> List[int]:
+        potions.sort()
+        return [
+            len(potions) - bisect_left(potions, success / spell) for spell in spells
+        ]
