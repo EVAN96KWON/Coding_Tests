f3d66da7e7ec93e862dbb37f2f56a688df4528d5
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-03-15 11:18:09 +0900

    Time: 27 ms (96.63%), Space: 16.5 MB (92.12%) - LeetHub

--- LeetCode/Medium/0399-evaluate-division/0399-evaluate-division.py
+++ LeetCode/Medium/0399-evaluate-division/0399-evaluate-division.py
@@ -11,19 +11,19 @@ class Solution:
             self.graph[i][j] = v
             self.graph[j][i] = 1 / v
 
-        return [self.dfs(i, j, set()) or -1.0 for i, j in queries]
+        return [self.bfs(i, j, set()) or -1.0 for i, j in queries]
 
-    def dfs(self, start, end, visited):
+    def bfs(self, start, end, visited):
         if start not in self.graph or end not in self.graph:
             return
 
-        stack = [(start, 1)]
-        while stack:
-            i, v = stack.pop()
+        queue = [(start, 1)]
+        while queue:
+            i, v = queue.pop(0)
             if i == end:
                 return v
             if i in visited:
                 continue
             visited.add(i)
             for j in self.graph[i]:
-                stack.append((j, v * self.graph[i][j]))
+                queue.append((j, v * self.graph[i][j]))
