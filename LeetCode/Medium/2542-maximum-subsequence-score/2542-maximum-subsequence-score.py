c05f7f3e311caa2d24359d74b314789868314c48
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-03-18 11:14:51 +0900

    Time: 787 ms (35.1%), Space: 37.8 MB (46.09%) - LeetHub

---
+++ LeetCode/Medium/2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
@@ -0,0 +1,16 @@
+from heapq import heappush, heappop
+
+
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        min_heap = []
+        total = res = 0
+
+        for n1, n2 in sorted(zip(nums1, nums2), key=lambda x: -x[1]):
+            total += n1
+            heappush(min_heap, n1)
+            if len(min_heap) > k:
+                total -= heappop(min_heap)
+            if len(min_heap) == k:
+                res = max(res, total * n2)
+        return res
