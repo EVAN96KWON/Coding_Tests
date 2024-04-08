900c87ec5e8f0c07018088b75f5937cfef668b2c
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-04-08 17:48:04 +0900

    [level 3] Title: 숫자 게임, Time: 48.05 ms, Memory: 19.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+
+def solution(A, B):
+    cnt = 0
+    A = deque(sorted(A))
+    B = deque(sorted(B))
+    while B:
+        if A[0] < B[0]:
+            cnt += 1
+            A.popleft(), B.popleft()
+        else:
+            B.popleft()
+    return cnt
