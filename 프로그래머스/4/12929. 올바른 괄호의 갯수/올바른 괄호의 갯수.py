545847694cfada2aa15cc25afd953b9ee4c26c62
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-04-08 17:26:50 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 3169.97 ms, Memory: 239 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -0,0 +1,13 @@
+def solution(n):
+    def backtrack(current, open_count, close_count):
+        if open_count == close_count == n:
+            result.append(current)
+            return
+        if open_count < n:
+            backtrack(current + "(", open_count + 1, close_count)
+        if close_count < open_count:
+            backtrack(current + ")", open_count, close_count + 1)
+
+    result = []
+    backtrack("", 0, 0)
+    return len(result)
