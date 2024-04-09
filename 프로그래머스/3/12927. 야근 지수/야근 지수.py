6f0ad59bf03280d325ffe2a3736313189e8a1cab
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-04-09 14:20:44 +0900

    [level 3] Title: 야근 지수, Time: 346.78 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -2,13 +2,7 @@ from heapq import heapify, heappush, heappop
 
 
 def solution(n, works):
-    works = [-x for x in works]
-    heapify(works)
-    for _ in range(n):
-        max_work = heappop(works)
-        if max_work == 0:
-            break
-        max_work += 1
-        heappush(works, max_work)
-
+    heapify(works := [-x for x in works])
+    for _ in range(min(n, -1 * sum(works))):
+        heappush(works, heappop(works) + 1)
     return sum(map(lambda x: x**2, works))
