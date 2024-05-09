85c3b9ad59fa08f3b3e54f89bae4579cf769b889
heonjin <87924725+kwon-evan@users.noreply.github.com>
2024-05-09 10:20:07 +0900

    [Silver I] Title: 음식물 피하기, Time: 268 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -2,15 +2,15 @@ from collections import deque
 
 
 def bfs(graph, start, visited):
+    dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     queue = deque([start])
     visited.add(start)
-    size = 1  # 현재 연결 요소의 크기
+    size = 1
 
     while queue:
         x, y = queue.popleft()
 
-        # 상하좌우 탐색
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        for dx, dy in dirs:
             nx, ny = x + dx, y + dy
             if (
                 0 <= nx < N
@@ -29,7 +29,6 @@ if __name__ == "__main__":
     N, M, K = map(int, input().split())
     graph = [[False for _ in range(M)] for _ in range(N)]
 
-    # 음식물 위치 표시
     for _ in range(K):
         r, c = map(int, input().split())
         graph[r - 1][c - 1] = True
@@ -40,7 +39,6 @@ if __name__ == "__main__":
     for i in range(N):
         for j in range(M):
             if graph[i][j] and (i, j) not in visited:
-                component_size = bfs(graph, (i, j), visited)
-                max_size = max(max_size, component_size)
+                max_size = max(max_size, bfs(graph, (i, j), visited))
 
     print(max_size)
